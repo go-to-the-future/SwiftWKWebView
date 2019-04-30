9e237d3d01e259b5881916245b967a02ccbd5f90
Kiyotan <appleumasugi.mjd@gmail.com>
2019-04-30 16:52:39 +0900

    Initial Commit

---
+++ SwiftWKWebView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftWKWebView
+//
+//  Created by Kiyoto Ryuman on 2019/04/30.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftWKWebViewTests/SwiftWKWebViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftWKWebViewTests.swift
+//  SwiftWKWebViewTests
+//
+//  Created by Kiyoto Ryuman on 2019/04/30.
+//  Copyright © 2019 Kiyoto Ryuman. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftWKWebView
+
+class SwiftWKWebViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
